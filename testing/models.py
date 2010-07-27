c91783d8c94de8d221bd2a26cbe503e1558ac47f
Tangerine Cat <kaineer@gmail.com>
2010-07-27 14:25:36 +0600

    Testing jabber notification

--- testing/models.py
+++ testing/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth import User
 
 # Create your models here.
+
+# Additional user information
+#
+#
+class Contact( User ):
+  user_id = models.ForeignKey( User )
+  jabber  = models.CharField( max_length = 50 )
+  # ... more to add
+
+
