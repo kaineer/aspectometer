bafbc1123c86d3347e604d1aa44e2af9c9db4201
root <root@vhost-stanis.(none)>
2010-08-03 04:06:47 +0000

    Files for django-admin-tools

---
+++ menu.py
@@ -0,0 +1,32 @@
+from django.core.urlresolvers import reverse
+from django.utils.translation import ugettext_lazy as _
+from admin_tools.menu import items, Menu
+
+# to activate your custom menu add the following to your settings.py:
+#
+# ADMIN_TOOLS_MENU = 'aspectometer.menu.CustomMenu'
+
+class CustomMenu(Menu):
+    """
+    Custom Menu for aspectometer admin site.
+    """
+    def __init__(self, **kwargs):
+        Menu.__init__(self, **kwargs)
+        self.children.append(items.MenuItem(
+            title=_('Dashboard'),
+            url=reverse('admin:index')
+        ))
+        self.children.append(items.AppList(
+            title=_('Applications'),
+            exclude_list=('django.contrib',)
+        ))
+        self.children.append(items.AppList(
+            title=_('Administration'),
+            include_list=('django.contrib',)
+        ))
+
+    def init_with_context(self, context):
+        """
+        Use this method if you need to access the request context.
+        """
+        pass
